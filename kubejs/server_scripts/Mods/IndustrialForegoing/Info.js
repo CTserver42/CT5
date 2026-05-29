3748cab5ab3021916bb3af0f645edc19686d5733
sh4dowfox4 <amerikapendosiya@gmail.com>
2026-05-29 20:58:04 +0500

    Delete old build

--- kubejs/server_scripts/Mods/IndustrialForegoing/Info.js
+++
@@ -1,6 +0,0 @@
-RecipeViewerEvents.addInformation('item', e => {
-  e.add('industrialforegoing:dryrubber', [
-    Text.translate('info.industrialforegoing.dryrubber.line1'),
-    Text.translate('info.industrialforegoing.dryrubber.line2')
-  ]);
-});
--- kubejs/server_scripts/Mods/Pastel/Enchants.js
+++
@@ -1,7 +0,0 @@
-// requires: pastel
-ServerEvents.tags('enchantment', e => {
-  e.remove('pastel:exclusive_set/indestructible', [
-    'minecraft:efficiency',
-    'minecraft:protection',
-  ]);
-});
